3a80b685da20bf8ffbdf53d32ae0666288ac198e
Passyah-studioalva <passyah@studioalva.co>
2023-01-03 09:58:01 +0700

    edited navbar

--- site/components/common/Head/Head.tsx
+++ site/components/common/Head/Head.tsx
@@ -1,17 +1,55 @@
-import type { VFC } from 'react'
-import { SEO } from '@components/common'
+import {
+  Box,
+  Flex,
+  Center,
+  Text,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Stack,
+  Heading,
+  Divider,
+  ButtonGroup,
+  Button,
+  Image,
+} from '@chakra-ui/react'
 
-const Head: VFC = () => {
+export default function Head() {
   return (
-    <SEO>
-      <meta
-        key="viewport"
-        name="viewport"
-        content="width=device-width, initial-scale=1"
-      />
-      <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
-    </SEO>
+    <nav>
+      <Box
+        boxShadow="xs"
+        p="40px"
+        rounded="md"
+        bg="white"
+        display="flex"
+        justify-items="center"
+        justifyContent="space-between"
+      >
+        <Image
+          borderRadius="full"
+          boxSize="20px"
+          src="https://cdn-icons-png.flaticon.com/512/2991/2991148.png"
+          alt="Logo"
+        />
+        <Flex gap={10} fontWeight="bold">
+          <Text>All</Text>
+          <Text>New Arrivals</Text>
+          <Text>Collection</Text>
+          <Text>Featured</Text>
+          <Text>Product</Text>
+        </Flex>
+        <Flex gap="20px" alignItems="center">
+          <Text>Login</Text>
+          <Image
+            borderRadius="full"
+            boxSize="20px"
+            src="https://cdn-icons-png.flaticon.com/512/3750/3750627.png"
+            alt="Logo"
+          />
+        </Flex>
+      </Box>
+    </nav>
   )
 }
-
-export default Head
